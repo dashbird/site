e50b2d79f99e0f04f270113a8eabc7cb66480655
adnanrahic <ado.raha198@gmail.com>
2018-06-16 00:46:10 +0200

    (refactor) image lazy load: added blazy dependency; now loading all heavy images with lazy load;

--- static/js/img-load.js
+++ static/js/img-load.js
@@ -1,6 +1,5 @@
-[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-  img.setAttribute('src', img.getAttribute('data-src'))
-  img.onload = function () {
-    img.removeAttribute('data-src')
-  }
-})
+/* global Blazy */
+;(() => {
+  const bLazy = new Blazy()
+  bLazy.revalidate()
+})()
