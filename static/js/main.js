b8c2de8b52c9672217b2f52a46f9430b8bf98e2a
Octavian Popa <octavian.popa@sprintsoft.ro>
2018-07-18 10:04:00 +0300

    Redesign blog

--- static/js/main.js
+++ static/js/main.js
@@ -36,4 +36,35 @@ $(document).ready(function () {
   $('ul.sidemenu li a').on('click', function () {
     $(this).parent('li').toggleClass('active')
   })
+
+
+  //Allows bootstrap carousels to display 3 items per page rather than just one
+  
+$('#carousel-example-multi').on('slide.bs.carousel', function (e) {
+
+  
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+  if($(window).width() > 767){
+    $('#carousel-example-multi').carousel({ 
+                  interval: 4000
+          });
+  }
+ 
 })
