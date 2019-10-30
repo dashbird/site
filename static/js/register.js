60b6c94c6f6916f58ecd5ea4ba46714478a7ef10
Taavi Rehemägi <taavirehemagi@gmail.com>
2019-10-30 19:13:23 +0200

    implement honeypot

--- static/js/register.js
+++ static/js/register.js
@@ -13,7 +13,8 @@ $(function () {
 
   const $registerForm = $formArray[0]
   const emailElement = $('#registerEmail')
-  const firstNameElement = $('input[name="firstName"]')
+  const firstNameElement = $('input[name="firstName"]') // for honeypot
+  const firstNameHoneyElement = $('input[name="firstname"]')
   const passwordElement = $('input[name="password"]')
   const confirmPasswordElement = $('input[name="confirmPassword"]')
 
@@ -26,15 +27,17 @@ $(function () {
     setTimeout(submitForm, 1000)
     var formSubmitted = false
     function submitForm () {
-      if (!formSubmitted) {
+      if (!formSubmitted && !firstNameHoneyElement.val()) {
         formSubmitted = true
         $registerForm.submit()
+      } else {
+        window.location.href = '/'
       }
     }
 
     gtag('event', 'signup-started', {
       'event_category': 'Signup',
-      'event_label': emailElement.value
+      'event_label': emailElement.val()
     })
     fbq('track', 'Lead', {
       value: 'signup-started'
