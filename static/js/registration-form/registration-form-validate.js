63994821bdfabd112b69dd3e4a49d58939500271
Teet Laja <teetlaja@iMac.lan>
2020-07-22 15:30:47 +0300

    finished validating blacklist e-mails

--- static/js/registration-form/registration-form-validate.js
+++ static/js/registration-form/registration-form-validate.js
@@ -1,36 +1,35 @@
-let emailInput = document.getElementById('registration-form-email')
-let emailInput.addEventListener('input', updateInputValue)
+// let form = document.getElementById('register-form')
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
-function addErrorClasses() {
-  emailInput.classList.add('is-invalid')
-}
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation')
 
-function remove errorClasses() {
-  emailInput.classList.remove('is-invalid')
-}
-
-function updateInputValue(e) {
-  console.log(e.target.value)
-}
-
-//Get value from email input
-
-
-//Check if domain is in blacklist
-function checkEmailFromBlacklist () {
-  let splitFormEmail = emailInput.split('@')
-  let isForbiddenEmail = blacklist.includes(splitFormEmail[1])
-  console.log(isForbiddenEmail)
-}
-
-function testValidation() {
-  console.log('tervist')
-}
-// Add error classes if domain is in blacklist
-
-
-// Remove errors if changing field
-
-
-//Prevent submitting before email is okay
-let registerSubmit = document.getElementById('register-form')
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        let emailInput = document.getElementById('registration-form-email').value
+        console.log(emailInput)
+        let splitFormEmail = emailInput.split('@')
+        console.log(splitFormEmail)
+        let isForbiddenEmail = blacklist.includes(splitFormEmail[1])
+        console.log(isForbiddenEmail)
+        if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+        } 
+        if (isForbiddenEmail) {
+          console.log('Is forbidden email')
+          event.preventDefault()
+          event.stopPropagation()
+          document.getElementById('registration-form-email').setCustomValidity('Please add business e-mail')
+        } else {
+          document.getElementById('registration-form-email').setCustomValidity('')
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  }, false)
+}())
