8eb6d4ac8769d18df571ae8cd533bd6d8d6bcd23
Teet Laja <teetlaja@iMac.lan>
2020-07-27 19:48:53 +0300

    updated function to

--- static/js/registration-form/registration-form-validate.js
+++ static/js/registration-form/registration-form-validate.js
@@ -1,37 +1,27 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
 
-  window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation')
-    // Loop over them and prevent submission
-    Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        let emailInput = document.getElementById('registration-form-email').value
-        console.log(emailInput)
-        let splitFormEmail = emailInput.split('@')
-        console.log(splitFormEmail)
-        let isForbiddenEmail = mailserversBlacklist.includes(splitFormEmail[1])
-        console.log(isForbiddenEmail)
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        } 
-        if (isForbiddenEmail) {
-          console.log('Is forbidden email')
-          event.preventDefault()
-          event.stopPropagation()
-          document.getElementById('registration-form-email').setCustomValidity('Please add business e-mail')
-        } else {
-          document.getElementById('registration-form-email').setCustomValidity('')
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  }, false)
-}())
 
-function onSubmit(token) {
-  document.getElementById('register-form').submit();
+function checkFormInputs (token, event) {
+  console.log('starting to run this function')
+  let emailInput = document.getElementById('email').value
+  console.log(emailInput)
+  let splitFormEmail = emailInput.split('@')
+  console.log(splitFormEmail)
+  let isForbiddenEmail = mailserversBlacklist.includes(splitFormEmail[1])
+  console.log(isForbiddenEmail)
+  if (isForbiddenEmail) {
+    grecaptcha.ready(function() {
+      grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
+        document.getElementById('email').classList.add('is-invalid')
+      });
+    });
+  }
+  if (!isForbiddenEmail) {
+    grecaptcha.ready(function() {
+      grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
+        console.log('starting to submit')
+        document.getElementById('register-form').submit()
+      });
+    });
+  }
 }
