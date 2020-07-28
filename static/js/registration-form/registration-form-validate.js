2e6d40a12b80d5b1e7cbaad18e735628c41758fd
Teet Laja <teet@digivabrik.ee>
2020-07-28 13:15:20 +0300

    made new struct for registration submitting

--- static/js/registration-form/registration-form-validate.js
+++ static/js/registration-form/registration-form-validate.js
@@ -1,27 +1,17 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 
-
-function checkFormInputs (token, event) {
-  console.log('starting to run this function')
+function submitRegistrationForm (token, event) {
   let emailInput = document.getElementById('email').value
-  console.log(emailInput)
   let splitFormEmail = emailInput.split('@')
-  console.log(splitFormEmail)
   let isForbiddenEmail = mailserversBlacklist.includes(splitFormEmail[1])
-  console.log(isForbiddenEmail)
-  if (isForbiddenEmail) {
-    grecaptcha.ready(function() {
-      grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
+  grecaptcha.ready(function () {
+    grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function() {
+      if (isForbiddenEmail) {
         document.getElementById('email').classList.add('is-invalid')
-      });
-    });
-  }
-  if (!isForbiddenEmail) {
-    grecaptcha.ready(function() {
-      grecaptcha.execute('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', {action: 'submit'}).then(function(token) {
-        console.log('starting to submit')
+      } else {
+        var recaptchaResponse = document.getElementById('recaptchaResponse');
+        recaptchaResponse.value = token;
         document.getElementById('register-form').submit()
-      });
+      }
     });
-  }
+  })
 }
